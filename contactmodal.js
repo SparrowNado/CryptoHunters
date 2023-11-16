6d9aff6ca5541d439fc04fa05b865107b71b6efe
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-11-17 00:06:18 +0800

    added  better navbar with burger

--- contactmodal.js
+++ contactmodal.js
@@ -1,15 +1,16 @@
-document.getElementById('contact').addEventListener('click', 
-function() {
-    document.querySelector('.contact-modal').style.display = 'flex';
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
 
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
 
-});
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
 
-document.querySelector('.close').addEventListener('click',
-function() {
-
-    document.querySelector('.contact-modal').style.display = 'none';
-
-
-
-});
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
